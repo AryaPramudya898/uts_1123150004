2294951a91f40fe065b1136129ee3926d63e5f11
AryaPramudya898 <aryapramudya610@gmail.com>
2026-04-20 09:53:13 +0700

    feat: membuat multiprovider

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,24 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:uts_1123150004/features/auth/presentation/providers/auth_provider.dart';
+import 'package:uts_1123150004/features/dashboard/presentation/providers/product_provider.dart';
 import 'package:uts_1123150004/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   // Inisialisasi Firebase SEBELUM runApp
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthProvider()),
+        ChangeNotifierProvider(create: (_) => ProductProvider()),
+      ],
+      child: const MyApp(),
+    ),
   );
-
-
-  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
