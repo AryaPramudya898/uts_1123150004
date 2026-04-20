2b8cc3bf13dbe86366f96641c63a4aa58e7899bc
AryaPramudya898 <aryapramudya610@gmail.com>
2026-04-20 09:22:16 +0700

    feat: init firebase di main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:uts_1123150004/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Inisialisasi Firebase SEBELUM runApp
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+
   runApp(const MyApp());
 }
 
